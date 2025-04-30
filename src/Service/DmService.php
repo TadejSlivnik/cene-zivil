1ba277f44630431701a61edbd4f1ee0a95e0ec80
Tadej Slivnik <tadej.slivnik@outlook.com>
2025-04-30 20:38:08 +0200

    fix: Include brand name in product title for better identification

--- src/Service/DmService.php
+++ src/Service/DmService.php
@@ -36,7 +36,7 @@ class DmService extends AbstractShopService
             $data[] = [
                 'source' => Product::SOURCE_DM,
                 'url' => 'https://www.dm.si/' . ltrim($item['relativeProductUrl'], '/'),
-                'title' => $item['brandName'] ?? '' . $item['title'],
+                'title' => ($item['brandName'] ?? '') . $item['title'],
                 'unit' => $unit,
                 'unitQuantity' => $unitQuantity,
                 'unitPrice' => $unitPrice,
