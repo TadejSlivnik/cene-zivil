61116489730455b024755af1eb35781551229406
Tadej Slivnik <tadej.slivnik@outlook.com>
2025-05-22 20:42:25 +0200

    nc

--- src/Command/SitemapCommand.php
+++ src/Command/SitemapCommand.php
@@ -27,7 +27,7 @@ class SitemapCommand extends AbstractCommand
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://www.cene-zivil.si/</loc>
+        <loc>https://www.cene-zivil.si</loc>
         <lastmod>{$today->format(\DateTime::ATOM)}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
